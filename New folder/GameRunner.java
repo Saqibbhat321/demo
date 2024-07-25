23503a08fa5aa086e07a7bfc04db898e805cd290
Saqibbhat321 <120316006+Saqibbhat321@users.noreply.github.com>
2024-07-25 13:42:43 +0530

    today

---
+++ New folder/GameRunner.java
@@ -0,0 +1,24 @@
+class GameRunner{
+	public static void main(String[] args)
+	{
+		String m1= "Cricket";
+		String m2 = "Football";
+		String m3= "Hockey";
+		String m4= "Kabadi";
+		String m5= "Basket Ball";
+		String m6= "Base Ball";
+		String m7= "kho kho";
+		String m8= "ring ball";
+		String m9= "howman";
+		String m10= "blues";
+
+		
+		String[] arrays ={m1,m2,m3,m4,m5,m6,m7,m8,m9,m10};
+		
+		
+		for(int index =0;index <arrays.length;index++)
+		{
+			System.out.println(arrays[index]);
+		}
+	}
+}
